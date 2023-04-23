0b8d6b00aa8f1eafbbcccd15c392b273519031f9
joshliu1121 <110278243+joshliu1121@users.noreply.github.com>
2023-04-23 12:16:51 +0800

    Update 開始遊戲以及離開遊戲選單完成

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;  //引用 Unity 引擎函式庫
+using UnityEngine.SceneManagement;
+
+namespace LSC 
+{
+    //腳本名稱:必須跟左上角檔案名稱一樣 包括大小寫 不能有空格
+    public class MenuManager : MonoBehaviour
+    {
+        // 方法Method
+        // 語法: 修飾詞 傳回類型 方法名稱()
+        // { 方法程式內容 }
+        public void StartGame() 
+        {
+            // 載入場景的API
+            SceneManager.LoadScene("遊戲場景");
+        }
+
+        public void QuitGame() 
+        {
+            // 離開遊戲的API
+            Application.Quit();
+        }
+    }
+}
