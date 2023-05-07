d1736362839be072fa265dd1716cc31dc61f4041
joshliu1121 <110278243+joshliu1121@users.noreply.github.com>
2023-05-07 12:59:21 +0800

    Update 怪物預置物和生成點製作

--- Assets/Script/SpawnSystem.cs
+++ Assets/Script/SpawnSystem.cs
@@ -1,9 +1,13 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace LSC
 {
     public class SpawnSystem : MonoBehaviour
     {
+        [Header("生成間隔"), Range(0, 10)]
+        public float interval = 3.5f;
 
+        [Header("生成怪物預置物")]
+        public GameObject prefabEnemy;
     }
 }
