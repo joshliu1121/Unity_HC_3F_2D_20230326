f649afb02fb30f649cab489818d6ba62abb77afa
joshliu1121 <110278243+joshliu1121@users.noreply.github.com>
2023-05-21 17:09:07 +0800

    Update 對話系統

--- Assets/Script/DamageSystem.cs
+++ Assets/Script/DamageSystem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 
 
 namespace LSC
@@ -8,12 +9,50 @@ namespace LSC
         [Header("血量資料")]
         public dataHealth data;
 
+        private float hp;
+
+        private DataHealthEnemy dataEnemy;
+
+        private void Awake()
+        {
+            hp = data.hp;
+            dataEnemy = (DataHealthEnemy)data;
+        }
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
             //print(collision.gameObject);
             if (collision.gameObject.name.Contains("武器"))
             {
-                print("被武器打到");
+                //print("被武器打到");
+                GetDamage();
+            }
+        }
+
+
+        private void GetDamage()
+        {
+            hp -= 50;
+            print("血量剩下:" + hp);
+
+            if (hp <= 0)
+            {
+                Dead();
+                DropExp();
+            }
+        }
+
+        private void Dead()
+        {
+            Destroy(gameObject);
+        }
+
+        private void DropExp()
+        {
+            // print("這隻怪物掉落的經驗值機率" + dataEnemy.dropProbability);
+            if (Random.value <= dataEnemy.dropProbability)
+            {
+                Instantiate(dataEnemy.prefabExp, transform.position, transform.rotation);
             }
         }
 
---
+++ Assets/Script/DataHealthEnemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace LSC
+{
+   [CreateAssetMenu(menuName = "LSC/DataHealthEnemy")]
+    public class DataHealthEnemy : dataHealth
+    {
+        [Header("掉落經驗值物件")]
+        public GameObject prefabExp;
+
+        [Header("掉落經驗值機率"), Range(0, 1)]
+        public float dropProbability;
+    
+    
+    
+    }
+}
